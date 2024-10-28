4be47963fc608af39a54bdd9517a2ed1024b4e50
Kevanana26 <525kpiccinich@frhsd.com>
2024-10-28 11:38:49 -0400

    my head hurts

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(){
+  const scheduleUrl = 'https://api.npoint.io/5e0bbb5d32a86ba8f280'
+
+
+  const bellSchedule = {
+    1: { start: '8:24 AM', end: '9:31 AM' },
+    2: { start: '9:36 AM', end: '10:43 AM' },
+    3: { start: '10:48 AM', end: '11:55 AM' },
+    4: { start: '12:41 PM', end: '1:48 PM' },
+    5: { start: '1:53 PM', end: '3:00 PM' }
+  }
+
+}
