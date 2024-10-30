3cd4e0451959bbb0f2ee0609f3ed3a41058856df
Kevanana26 <525kpiccinich@frhsd.com>
2024-10-30 10:33:22 -0400

    my headache is back! :(

--- script.js
+++ script.js
@@ -9,5 +9,33 @@ $(document).ready(){
     4: { start: '12:41 PM', end: '1:48 PM' },
     5: { start: '1:53 PM', end: '3:00 PM' }
   }
+$('#submitDay').on('click', function() {
+  const selectedDay = $('#dayInput').val().toUpperCase()
 
+
+
+
+  if (!['A', 'B', 'C', 'D', 'E', 'F', 'G'].includes(selectedDay)) {
+    alert('Please enter a valid day (A-G)')
+    return
+  }
+$.ajax(){
+  url = 'https://api.npoint.io/5e0bbb5d32a86ba8f280'
+  const aDayClasses = getClassesForDay('A');
+console.log(aDayClasses);
+const bDayClasses = getClassesForDay('B');
+console.log(bDayClasses);
+const cDayClasses = getClassesForDay('C');
+console.log(cDayClasses);
+const dDayClasses = getClassesForDay('D');
+console.log(dDayClasses);
+const eDayClasses = getClassesForDay('E');
+console.log(eDayClasses);
+const fDayClasses = getClassesForDay('F');
+console.log(fDayClasses);
+const gDayClasses = getClassesForDay('G');
+console.log(gDayClasses);
+}
+}
+)
 }
